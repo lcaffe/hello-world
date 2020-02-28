eb4ac8ed98b84ee9b5d80c5780e6b7b554052809
lcaffe <lcaffe@gmail.com>
2020-02-28 17:50:53 -0300

    Create Cliente.java

---
+++ hello-world/src/main/java/br/com/lcaffe/helloworld/model/Cliente.java
@@ -0,0 +1,26 @@
+package br.com.lcaffe.helloworld.model;
+
+public class Cliente {
+
+ 
+	private Integer numero;
+	private String nome;
+
+
+ public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
