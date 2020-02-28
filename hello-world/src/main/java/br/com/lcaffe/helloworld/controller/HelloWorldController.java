4c535dc50bdd7178d1888d930bc74dfcffcd4ad5
lcaffe <lcaffe@gmail.com>
2020-02-28 17:48:40 -0300

    Update HelloWorldController.java

--- hello-world/src/main/java/br/com/lcaffe/helloworld/controller/HelloWorldController.java
+++ hello-world/src/main/java/br/com/lcaffe/helloworld/controller/HelloWorldController.java
@@ -3,15 +3,26 @@ package br.com.lcaffe.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import br.com.lcaffe.helloworld.model.Cliente;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping(value="/hello", produces=MediaType.APPLICATION_JSON_VALUE)
 public class HelloWorldController {
 
 	@GetMapping
 	public String saudacao() {
 		return "Hello World";
 	}
+	
+   @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void adiciona(@RequestBody Cliente cliente) {
+	System.out.println("Cliente enviado...");
+    }
 	
 	
 }
